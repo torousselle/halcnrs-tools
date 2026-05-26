98c08e4651cd0a84261f71abadd4d7b00b8370a7
Théo Rousselle <theo.rousselle@inist.fr>
2026-05-26 15:12:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 pages = {
     "Codes": [
         st.Page("list_authors_struct.py", title="Extraction des auteurs d'un code structure"),
-        st.Page("licence_chart.py", title="Répartition des dépôts par licence")
+        st.Page("licence_chart.py", title="Répartition des dépôts par licence"),
+        st.Page("notice_texte_chart.py", title="Evolution du type de dépôt par date de publi")
     ]
 }
 
